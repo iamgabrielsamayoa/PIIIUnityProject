e4bf439a30b069dcb3cc757f9bc212ddac740bdc
Gabriel Samayoa <63943467+iamgabrielsamayoa@users.noreply.github.com>
2021-05-21 23:50:09 -0600

    Add files via upload

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    private static GameHandler instance;
+
+    private static int score;
+
+    [SerializeField] private Snake snake;
+
+    private LevelGrid levelGrid;
+
+    private void Awake() {
+        instance = this;
+        InitializeStatic();
+    }
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+
+        levelGrid = new LevelGrid(20, 20);
+
+        snake.Setup(levelGrid);
+        levelGrid.Setup(snake);
+
+        /*CMDebug.ButtonUI(Vector2.zero, "Reload Scene", () => {
+            Loader.Load(Loader.Scene.GameScene);
+        });*/
+    }
+
+
+
+    private static void InitializeStatic() {
+        score = 0;
+    }
+
+    public static int GetScore() {
+        return score;
+    }
+
+    public static void AddScore() {
+        score += 1;
+    }
+
+}
